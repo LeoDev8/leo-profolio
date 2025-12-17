75bba2d2375155c02c2d08a263a013aefb337cce
Leo <zhihao_2023@163.com>
2025-12-17 17:39:33 +0800

    Aplly Times New Roman Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import localFont from "next/font/local";
+
+const timesSans = localFont({
+  src: "./fonts/times.ttf",
+  variable: "--font-times",
+  weight: "100 200 300 400 500 600 700 800 900",
+});
+const kaitiSans = localFont({
+  src: "./fonts/Kaiti_GB2312.ttf",
+  variable: "--font-kaiti",
+  weight: "100 200 300 400 500 600 700 800 900",
+});
 
 export const metadata: Metadata = {
   title: "Leo Profolio",
@@ -13,7 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${timesSans.variable} ${kaitiSans.variable} font-times antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
