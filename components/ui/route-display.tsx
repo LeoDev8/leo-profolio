c3a8973d0d9557abbc2dd0eeed310335653213aa
Leo <zhihao_2023@163.com>
2025-12-21 19:32:37 +0800

    Add an icon.ico(Need to change)

--- components/ui/route-display.tsx
+++ components/ui/route-display.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function RouteDisplay({ lang }: { lang: String }) {
-  const pathname = usePathname();
+  // const {pathname, setPathname} = useState("")
+  const splitedPathname = usePathname().split("/");
+  console.log(`From route-display.tsx: ${splitedPathname.length}`);
+  if (splitedPathname.length <= 2) {
+
+  }
 
   return <span>Route Display</span>;
 }
